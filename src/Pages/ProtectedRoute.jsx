e5bdfd61262cf612f80b6984009322722af59189
Saakshi Sharma <saakshi7466@gmail.com>
2026-03-01 11:41:43 +0530

    final

--- src/Pages/ProtectedRoute.jsx
+++ src/Pages/ProtectedRoute.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Navigate } from "react-router-dom";
-import toast from 'react-hot-toast';
+import toast from "react-hot-toast";
+
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("token"); // Make sure this matches where you store your token
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    if (!token) {
+      toast.error("Please login first 🔐");
+    }
+  }, [token]);
 
   if (!token) {
-    // Redirect to login page if not logged in
-    toast.error("Please login first 🔐");
     return <Navigate to="/" replace />;
   }
 
-  return children; // Render the protected component
+  return children;
 };
 
 export default ProtectedRoute;
