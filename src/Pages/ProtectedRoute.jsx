2723cf0060170858b0dd07f08c8180a4f9178bc3
Saakshi Sharma <saakshi7466@gmail.com>
2026-03-01 11:40:04 +0530

    final

--- src/Pages/ProtectedRoute.jsx
+++ src/Pages/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-
+import toast from 'react-hot-toast';
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token"); // Make sure this matches where you store your token
 
   if (!token) {
     // Redirect to login page if not logged in
+    toast.error("Please login first 🔐");
     return <Navigate to="/" replace />;
   }
 
